143e74e7fc54a222072ab715ee7f697dddb124fd
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-15 07:38:53 +0530

    define setters/getters/constructor for User and Initialize data in UserDataMap

---
+++ src/main/java/com/gfg/yogi/SpringMVCDemo/data/UserDataMap.java
@@ -0,0 +1,26 @@
+package com.gfg.yogi.SpringMVCDemo.data;
+
+import com.gfg.yogi.SpringMVCDemo.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDataMap {
+
+    public static Map<Long, User> userData = new HashMap<>();
+
+    static {
+
+        User user0 = new User(0L,"Rod","Johnson");
+        User user1 = new User(1L , "Eric", "Evans");
+        User user2 = new User(2L,"Bill","Gates");
+        User user3 = new User(3L,"Nelson","Mandela");
+
+        userData.put(user0.getId(),user0);
+        userData.put(user1.getId(),user1);
+        userData.put(user2.getId(),user2);
+        userData.put(user3.getId(),user3);
+
+    }
+
+}
